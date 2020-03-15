8f9c926f47141542be648fe182fd34c4b2fddb5c
jaskiewicz <jaskiewicz@DEM3852>
2020-03-15 12:23:53 +0100

    SignUp made

--- PazumAniaWebService/Models/Idea.cs
+++ PazumAniaWebService/Models/Idea.cs
@@ -7,6 +7,7 @@ namespace PazumAniaWebService.Models
 {
     public class Idea
     {
+        //KoronaWirus
         public int Id { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
